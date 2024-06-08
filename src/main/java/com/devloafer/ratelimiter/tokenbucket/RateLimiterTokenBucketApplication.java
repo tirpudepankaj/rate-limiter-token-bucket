10e641ec673b1822212d3f9961509a0de4c68e5f
Pankaj <tirpudepankaj@gmail.com>
2024-06-08 16:29:01 +0530

    Initial Commit

---
+++ src/main/java/com/devloafer/ratelimiter/tokenbucket/RateLimiterTokenBucketApplication.java
@@ -0,0 +1,13 @@
+package com.devloafer.ratelimiter.tokenbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterTokenBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterTokenBucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devloafer/ratelimiter/tokenbucket/RateLimiterTokenBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devloafer.ratelimiter.tokenbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterTokenBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
